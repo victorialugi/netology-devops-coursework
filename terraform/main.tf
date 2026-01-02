251135ee3b96a93be9fbc1c551680a9b1316f39e
victorialugi <victorialuginina@gmail.com>
2026-01-02 15:47:19 +0000

    fix: temporarily allow SSH from anywhere for coursework convenience

---
+++ terraform/main.tf
@@ -0,0 +1,107 @@
+data "yandex_compute_image" "ubuntu" {
+  family = "ubuntu-2204-lts"
+}
+
+# VPC
+resource "yandex_vpc_network" "coursework" {
+  name = "coursework-net"
+}
+
+# Подсети
+resource "yandex_vpc_subnet" "public-a" {
+  name           = "public-a"
+  zone           = "ru-central1-a"
+  network_id     = yandex_vpc_network.coursework.id
+  v4_cidr_blocks = ["192.168.10.0/24"]
+}
+
+resource "yandex_vpc_subnet" "private-a" {
+  name           = "private-a"
+  zone           = "ru-central1-a"
+  network_id     = yandex_vpc_network.coursework.id
+  v4_cidr_blocks = ["192.168.20.0/24"]
+}
+
+resource "yandex_vpc_subnet" "private-b" {
+  name           = "private-b"
+  zone           = "ru-central1-b"
+  network_id     = yandex_vpc_network.coursework.id
+  v4_cidr_blocks = ["192.168.30.0/24"]
+}
+
+# Security Groups
+resource "yandex_vpc_security_group" "sg-bastion" {
+  name       = "sg-bastion"
+  network_id = yandex_vpc_network.coursework.id
+
+  ingress {
+    protocol       = "TCP"
+    description    = "SSH из интернета (временно)"
+    port           = 22
+    v4_cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    protocol       = "ANY"
+    from_port      = 0
+    to_port        = 65535
+    v4_cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "yandex_vpc_security_group" "sg-private" {
+  name       = "sg-private"
+  network_id = yandex_vpc_network.coursework.id
+
+  ingress {
+    protocol          = "TCP"
+    description       = "SSH только с bastion"
+    port              = 22
+    security_group_id = yandex_vpc_security_group.sg-bastion.id
+  }
+
+  egress {
+    protocol       = "ANY"
+    from_port      = 0
+    to_port        = 65535
+    v4_cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+# Bastion host
+resource "yandex_compute_instance" "bastion" {
+  name        = "bastion"
+  zone        = "ru-central1-a"
+  platform_id = "standard-v3"
+
+  resources {
+    cores         = 2
+    memory        = 4
+    core_fraction = 20
+  }
+
+  boot_disk {
+    initialize_params {
+      image_id = data.yandex_compute_image.ubuntu.image_id
+      size     = 20
+      type     = "network-ssd"
+    }
+  }
+
+  network_interface {
+    subnet_id          = yandex_vpc_subnet.public-a.id
+    nat                = true
+    security_group_ids = [yandex_vpc_security_group.sg-bastion.id]
+  }
+
+  metadata = {
+    user-data = <<-EOF
+      #cloud-config
+      users:
+        - name: ubuntu
+          sudo: ALL=(ALL) NOPASSWD:ALL
+          ssh_authorized_keys:
+            - ${var.ssh_public_key}
+      EOF
+  }
+}
